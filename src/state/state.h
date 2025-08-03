712f919d9907498eb878ab35bb162d19cccad4d3
Daniel Jeremiah <danielshunom2@gmail.com>
2025-08-03 08:15:37 -0400

    set experiment value to nullable

--- src/state/state.h
+++ src/state/state.h
@@ -8,10 +8,23 @@
 typedef struct {
   bool               APP_SESSION_alive;
   bool               APP_SESSION_active;
-  uint8_t            APP_SESSION_active_exp;
+  uint8_t*           APP_SESSION_active_exp;
   char*              APP_SESSION_current_user;
   ExperimentSession* APP_SESSION_current_exp_session;
   MetaData*          APP_SESSION_metadata; 
   Queue              APP_SESSION_exp_logs_queue;
   FILE*              APP_SESSION_logFile;
 } AppContext;
+
+static inline AppContext StartApplication() {
+  return (AppContext) {
+    .APP_SESSION_alive               = true,
+    .APP_SESSION_active              = false,
+    .APP_SESSION_active_exp          = NULL,
+    .APP_SESSION_current_user        = NULL,
+    .APP_SESSION_current_exp_session = NULL,
+    .APP_SESSION_metadata            = NULL,
+    .APP_SESSION_exp_logs_queue      = new_queue(),
+    .APP_SESSION_logFile             = NULL,
+  };
+}
