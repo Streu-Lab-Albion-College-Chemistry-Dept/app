b39053d9aba29ad0608a355973f8f4e101baff6b
Daniel Jeremiah <danielshunom2@gmail.com>
2025-08-04 11:57:21 -0400

    updated components

--- src/models/components/components.c
+++ src/models/components/components.c
@@ -1,15 +1,18 @@
-#include "raylib.h"
+#include "../../../include/clay/clay.h"
 #include "./components.h"
+#include <stdlib.h>
 
 /*
  * Top Menu Bar
 */
 void RenderHeaderButton(Clay_String text, AppContext* ctx) {
   CLAY({
-    .layout = {.padding = {16, 8}},
-    .backgroundColor = {},
-    .cornerRadius = {12, 12, 12, 12},
-    .backgroundColor = {140, 140, 140, 255},
+    .layout = {
+      .padding = {8, 8, 8, 8},
+      .childAlignment = {.y = CLAY_ALIGN_Y_CENTER},
+    },
+    .cornerRadius = {4, 4, 4, 4},
+    .backgroundColor = {165, 205, 125, 255},
   }) {
     CLAY_TEXT(text, CLAY_TEXT_CONFIG({
       .fontSize = 16,
@@ -20,8 +23,12 @@ void RenderHeaderButton(Clay_String text, AppContext* ctx) {
 
 void RenderHeaderDropdownButton(Clay_String text, AppContext* ctx) {
   CLAY({
-    .layout = {.padding = {16, 8}},
-    .cornerRadius = {5, 5, 5, 5},
+    .id = CLAY_ID("file-menu-button"),
+    .layout = {
+      .padding = {8, 8, 8, 8},
+      .sizing = {.width = CLAY_SIZING_GROW(0)}
+    },
+    .cornerRadius = {4, 4, 4, 4},
     .backgroundColor = {140, 140, 140, 255},
   }) {
     CLAY_TEXT(text, CLAY_TEXT_CONFIG({
@@ -33,62 +40,108 @@ void RenderHeaderDropdownButton(Clay_String text, AppContext* ctx) {
 
 void SideBar(AppContext* ctx) {
   Clay_Sizing expandLayout = {
-    .height = CLAY_SIZING_GROW(),
-    .width  = CLAY_SIZING_GROW(),
-  }
+    .height = CLAY_SIZING_GROW(0),
+    .width  = CLAY_SIZING_GROW(0),
+  };
   
   CLAY({ 
     .id = CLAY_ID("SideBar"),
     .layout = {
       .layoutDirection = CLAY_TOP_TO_BOTTOM,
       .childGap        = 16,
-      .padding         = {4, 4, 4, 4},
+      .padding         = {8, 8, 8, 8},
       .sizing = {
         .width  = CLAY_SIZING_FIXED(250),
-        .height = CLAY_SIZING_GROW();
+        .height = CLAY_SIZING_GROW(0),
       }
     },
-    .backgroundColor = APP_COLOR_sideBar,
+    .backgroundColor = {105, 48, 140, 255},
     .cornerRadius = {12, 12, 12, 12},
   }) {
     CLAY({
-      .id = CLAY_ID("title-bar"),
+      .cornerRadius = {8, 8, 8, 8},
+      .backgroundColor = {23, 45, 56, 200},
+      .id = CLAY_ID("info-bar"),
       .layout = {
+        .layoutDirection = CLAY_TOP_TO_BOTTOM,
+        .padding = {8, 8, 8, 8},
         .sizing = {
-          .height = CLAY_SIZING_FIXED(60),
-          .width  = CLAY_SIZING_FIXED(),
+          .height = CLAY_SIZING_FIXED(242),
+          .width  = CLAY_SIZING_GROW(0),
         }
       },
-    }) {};
+    }) {
+      Clay_String appActivity = 
+         ctx->APP_SESSION_alive
+         ? CLAY_STRING("Application: On")
+         : CLAY_STRING("Application: Off");
+
+      Clay_String sessionActivity = 
+        ctx->APP_SESSION_active
+        ? CLAY_STRING("Ongoing Session")
+        : CLAY_STRING("No active session");
+
+      Clay_String activeUser = 
+        ctx->APP_SESSION_current_user
+        ? CLAY_STRING("User: Lab Member")
+        : CLAY_STRING("User: system");
+
+      CLAY_TEXT(appActivity, CLAY_TEXT_CONFIG({
+        .fontSize  = 16,
+        .textColor = {255, 255, 255, 255},
+      }));
+      
+      CLAY_TEXT(activeUser, CLAY_TEXT_CONFIG({
+        .fontSize  = 20,
+        .textColor = {123, 233, 86, 105},
+      }));
+
+      CLAY_TEXT(sessionActivity, CLAY_TEXT_CONFIG({
+        .fontSize  = 16,
+        .textColor = {255, 255, 255, 255},
+      }));
+    }
     
     CLAY({
       .id = CLAY_ID("exp-configs"),
+      .cornerRadius = {8, 8, 8, 8},
+      .backgroundColor = {13, 34, 23, 150},
       .layout = {
-        .sizing = expandLayout,
-      } 
-    }) {}
+        .sizing  = expandLayout,
+        .layoutDirection = CLAY_TOP_TO_BOTTOM,
+        .childGap = 16,
+        .padding = {8, 8, 8, 8},
+      },
+    }) {
+      RenderHeaderButton(CLAY_STRING("Dr Streu San"), ctx);
+      CLAY_TEXT(CLAY_STRING("PI"), CLAY_TEXT_CONFIG({
+        .fontSize = 16,
+        .textColor = {255, 255, 255, 255},
+      }));
+    }
   };
 }
 
 void TopMenuBar(AppContext* ctx) {
   CLAY({
-    .backgroundColor = APP_COLOR_sideBar,
+    .backgroundColor = {130, 35, 180, 255},
     .cornerRadius = {12, 12, 12, 12},
     .id = CLAY_ID("top-menu-bar"),
     .layout = {
       .layoutDirection = CLAY_LEFT_TO_RIGHT,
-      .padding = {4, 4, 4, 4},
+      .padding = {8, 8, 8, 8},
       .sizing = {
-        .height = CLAY_SIZING_FIXED(60),
-        .width  = CLAY_SIZING_GROW(),
+        .width  = CLAY_SIZING_GROW(0),
       },
       .childGap = 16,
       .childAlignment = {.y = CLAY_ALIGN_Y_CENTER},
-    }
+    },
   }) {
     CLAY({
       .id = CLAY_ID("file-button"),
-      .layout = {.padding = {16, 8},},
+      .layout = {
+        .padding = {8, 8, 8, 8},
+      },
       .backgroundColor = {140, 140, 140, 255},
       .cornerRadius = {4, 4, 4, 4},
     }) {
@@ -97,24 +150,58 @@ void TopMenuBar(AppContext* ctx) {
           .textColor = {255, 255, 255, 255},
       }));
 
-      CLAY({
-        .id = CLAY_ID("file-button"),
-        .floating = {},
-        .backgroundColor = {40, 40, 40, 255},
-        .layout = {
-          .layoutDirection = CLAY_TOP_TO_BOTTOM,
-          .sizing = {.width = CLAY_SIZING_FIXED(200)} 
+      bool fileMenuVisible = 
+        Clay_PointerOver(Clay_GetElementId(CLAY_STRING("file-menu")))
+        ||
+        Clay_PointerOver(Clay_GetElementId(CLAY_STRING("file-button")));
+
+      if (fileMenuVisible) {
+         CLAY({
+          .id = CLAY_ID("file-menu"),
+          .floating = {
+            // .offset = {.y = 8},
+            .attachTo = CLAY_ATTACH_TO_PARENT,
+            .attachPoints = {
+              .parent = CLAY_ATTACH_POINT_LEFT_BOTTOM,
+            }
+          },
+          .cornerRadius = {8, 8, 8, 8},
+          .backgroundColor = {40, 40, 40, 255},
+          .layout = {
+            .padding = {0, 0, 8, 8},
+            .layoutDirection = CLAY_TOP_TO_BOTTOM,
+            .padding = {10, 10, 10, 10},
+            .childGap = 8,
+            .sizing = {.width = CLAY_SIZING_FIXED(200)} 
+          },
+        }) {
+          RenderHeaderDropdownButton(CLAY_STRING("New"), ctx);
+          RenderHeaderDropdownButton(CLAY_STRING("Open"), ctx);
+          RenderHeaderDropdownButton(CLAY_STRING("Save"), ctx);
+          RenderHeaderDropdownButton(CLAY_STRING("Close"), ctx);
         }
-      }) {
-        RenderHeaderDropdownButton(CLAY_STRING("New"), ctx);
-        RenderHeaderDropdownButton(CLAY_STRING("Open"), ctx);
-        RenderHeaderDropdownButton(CLAY_STRING("Save"), ctx);
-        RenderHeaderDropdownButton(CLAY_STRING("Close"), ctx);
       }
     }
+    
+    CLAY({.layout = {.sizing = {.width = CLAY_SIZING_GROW()}}}) {};
     RenderHeaderButton(CLAY_STRING("File"), ctx);
     RenderHeaderButton(CLAY_STRING("Edit"), ctx);
-    CLAY({.layout = {.sizing = {.width = CLAY_SIZING_GROW()}}});
     RenderHeaderButton(CLAY_STRING("Settings"), ctx);
   }
+}
+
+void MainContent(AppContext* ctx) {
+  CLAY({
+    .id = CLAY_ID("main-content"),
+    .clip = {.vertical = true, .childOffset = Clay_GetScrollOffset()},
+    .cornerRadius = {12, 12, 12, 12},
+    .backgroundColor = {130, 145, 34, 200},
+    .layout = {
+      .layoutDirection = CLAY_TOP_TO_BOTTOM,
+      .sizing = {
+        .width  = CLAY_SIZING_GROW(0),
+        .height = CLAY_SIZING_GROW(0),
+      },
+    }
+  }) {}
 }
--- src/models/components/components.h
+++ src/models/components/components.h
@@ -1,18 +1,19 @@
-#define CLAY_IMPLEMENTATION
+#pragma once
 #include "../../state/state.h"
 #include "../../../include/clay/clay.h"
 
-const Clay_Color APP_COLOR_LIGHT = (Clay_Color) {225, 220, 222, 1};
-const Clay_Color APP_COLOR_DARK  = (Clay_Color) {25, 100, 106, 1};
-const Clay_Color APP_COLOR_sideBar = (Clay_Color) {255, 248, 240, 1};
-const Clay_Color APP_COLOR_buttonRun = (Clay_Color) {35, 12, 51, 1};
+static const Clay_Color APP_COLOR_LIGHT     __attribute__((unused)) = {225, 220, 222, 255};
+static const Clay_Color APP_COLOR_DARK      __attribute__((unused)) = {25, 100, 106, 255};
+static const Clay_Color APP_COLOR_sideBar   __attribute__((unused)) = {255, 248, 240, 255};
+static const Clay_Color APP_COLOR_buttonRun __attribute__((unused)) = {35, 12, 51, 255};
 
 void SideBar(AppContext* ctx);
 void TopMenuBar(AppContext* ctx);
+void MainContent(AppContext* ctx);
 void RenderHeaderButton(Clay_String text, AppContext* ctx);
 void RenderHeaderDropdownButton(Clay_String text, AppContext* ctx);
 void ExpConfig(AppContext* ctx);
 void RelayMods(AppContext* ctx);
 void LogDisplDesc(AppContext* ctx);
 void LogDispl(AppContext* ctx);
-
+void ExperimentVisualContainer(AppContext* ctx);
